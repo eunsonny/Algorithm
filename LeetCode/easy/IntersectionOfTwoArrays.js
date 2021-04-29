86d84c91c43e01023240ab2b21fbd8d538c11232
eunsunShin <unusual0218@gmail.com>
2021-04-29 23:16:19 +0900

    2021-04-29 codekata

---
+++ LeetCode/easy/IntersectionOfTwoArrays.js
@@ -0,0 +1,37 @@
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+// Example 2:
+
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
+// Explanation: [9,4] is also accepted.
+
+// answer
+
+var intersect = function (nums1, nums2) {
+  nums1.sort((a, b) => {
+    return a - b;
+  });
+  nums2.sort((a, b) => {
+    return a - b;
+  });
+  let i = 0;
+  let j = 0;
+  const result = [];
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      result.push(nums1[i]);
+      i++;
+      j++;
+    } else if (nums1[i] < nums2[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+  return result;
+};
