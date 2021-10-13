da5d8a308221c44a6812f0cfe03abf707b29eb54
eunsunshin <unusual0218@gmail.com>
2021-10-13 16:56:50 +0900

    2021-10-13 codekata

---
+++ LeetCode/easy/ImplementStrStr.js
@@ -0,0 +1,46 @@
+// Implement strStr().
+
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Clarification:
+
+// What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+// For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+
+
+// Example 1:
+// Input: haystack = "hello", needle = "ll"
+// Output: 2
+
+// Example 2:
+// Input: haystack = "aaaaa", needle = "bba"
+// Output: -1
+
+// Example 3:
+// Input: haystack = "", needle = ""
+// Output: 0
+
+// answer
+
+var strStr = function (haystack, needle) {
+  if (needle === '' || needle === haystack) return 0;
+  if (haystack.length < needle.length) return -1;
+
+  for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+    if (haystack[i] === needle[0]) {
+      for (let j = 0; j < needle.length; j++) {
+        if (needle[j] !== haystack[i + j]) {
+          break;
+        } else if (j === needle.length - 1) {
+          return i;
+        }
+      }
+    }
+  }
+
+  return -1;
+};
+
+
